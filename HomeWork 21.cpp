2892cecca8a373ffc266cb1f737da9cfe6d9184f
User <User@WIN-D6I7BGDFHRA>
2022-07-10 10:46:59 +0200

    Добавьте файлы проекта.

---
+++ HomeWork 21.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+
+
+void ShowLine(int num, char sym, bool horver)
+{
+    if (horver == true)
+    {
+        std::cout << sym;
+    }
+    else if (horver == false)
+    {
+        std::cout << sym << "\n";
+    }
+
+
+    if (num > 0)
+    {
+        num--;
+        ShowLine(num, sym, horver);
+    }
+}
+
+
+long int Cube(int number)
+{
+    return number * number * number;
+}
+
+
+std::string IsPrime(int prime)
+{
+    for (int sanya = 2; sanya < 10; sanya++)
+    {
+        if (prime != sanya)
+        {
+            if (prime % sanya == 0)
+            {
+                return "\n\nFALSE";
+            }
+        }
+    }
+    return "\n\nTRUE";
+
+}
+
+
+int main()
+{
+    ShowLine(20, '@', true);
+    std::cout << "\n\n" << Cube(5);
+    std::cout << IsPrime(17);
+}
